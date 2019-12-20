bc9df24506bc74dc3b33f0be8e8316a52b7f86d7
Milan Shah <milanshah1@gmail.com>
2019-12-20 21:06:47 +0530

    Try prod db fix 2

--- events/models.py
+++ events/models.py
@@ -26,7 +26,7 @@ class Organizer(models.Model):
 
     name = models.CharField(max_length=32)
     phone = models.CharField(max_length=32)
-    email = models.EmailField()
+    email = models.EmailField(blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -35,7 +35,7 @@ class Organizer(models.Model):
 class Event(models.Model):
 
     name = models.CharField(max_length=64)
-    desc = models.TextField(max_length=2000)
+    desc = models.TextField(max_length=4000)
     price = models.DecimalField(
         max_digits=10, decimal_places=2, null=True, blank=True)
     organizer_1 = models.ForeignKey(
